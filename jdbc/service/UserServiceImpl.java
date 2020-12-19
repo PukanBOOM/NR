0891656361f58e030fa29a78cc6d8b26eda3442a
PukanBOOM <73070968+PukanBOOM@users.noreply.github.com>
2020-12-19 16:58:54 +0300

    Update UserServiceImpl.java

--- jdbc/service/UserServiceImpl.java
+++ jdbc/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-   private UserDao userDao = new UserDaoHibernateImpl();
+   private UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
         userDao.createUsersTable();
     }
